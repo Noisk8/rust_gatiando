a170c7bdb4bf96c1e7df206b18820d5d867ba955
Noisk8 <juansk8jaramillo@gmail.com>
2023-08-05 16:47:30 -0500

    nea2

---
+++ target/debug/deps/juanbareta-06884261c488e85c.d
@@ -0,0 +1,5 @@
+/home/noisk8/rust_gatiando/target/debug/deps/juanbareta-06884261c488e85c.rmeta: src/main.rs
+
+/home/noisk8/rust_gatiando/target/debug/deps/juanbareta-06884261c488e85c.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/juanbareta-7d5aab97776ccba9.d
@@ -0,0 +1,5 @@
+/home/noisk8/rust_gatiando/target/debug/deps/juanbareta-7d5aab97776ccba9.rmeta: src/main.rs
+
+/home/noisk8/rust_gatiando/target/debug/deps/juanbareta-7d5aab97776ccba9.d: src/main.rs
+
+src/main.rs:
